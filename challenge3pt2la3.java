2f28eff5184d920635c139c01868117cf0a2487e
Ashw-in2006 <rajaashwin2006@gmail.com>
2025-08-19 22:08:19 +0530

    Updated challenge3 example

--- challenge3pt2la3.java
+++ challenge3pt2la3.java
@@ -12,3 +12,20 @@ class challenge3pt2la3 {
 
     }
 } 
+
+/*import java.util.Scanner;
+class q4{
+    public static void main(String arg [])
+    {
+Scanner scan = new Scanner(System.in);
+int size = scan.nextInt();
+int[] marks = new int[size];
+
+for(int i=0;i<=size-1;i=i+1)
+{
+    marks[i] = scan.nextInt();
+}
+    System.out.print("The Middle element in the array is:");
+    System.out.print(marks[size/2]);
+}
+}*/
