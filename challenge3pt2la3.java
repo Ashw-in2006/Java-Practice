c836932bcd52afcc16ed8ff7997b80a5ae260187
Ashw-in2006 <rajaashwin2006@gmail.com>
2025-08-19 22:03:05 +0530

    Updated challenge3 example

---
+++ challenge3pt2la3.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+class challenge3pt2la3 {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        int[] marks = new int[size];
+
+        for(int i = 0 ; i<=size-1 ; i=i+1){
+            marks [i] = scan.nextInt();
+        }
+
+    }
+} 
